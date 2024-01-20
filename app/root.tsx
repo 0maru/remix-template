e4a75ce0ccfa7ec30776a591a201badc1d6966c3
Toi Yoshimaru <0maru.dev@gmail.com>
2024-01-21 03:20:16 +0900

    fix indent

--- app/root.tsx
+++ app/root.tsx
@@ -3,16 +3,16 @@ import { Links, Meta, Outlet, Scripts } from '@remix-run/react'
 const Root = () => {
   return (
     <html lang={'ja'}>
-    <head>
-      <link rel="icon" href="data:image/x-icon;base64,AA" />
-      <Meta />
-      <Links />
-    </head>
-    <body>
-    <Outlet />
+      <head>
+        <link rel="icon" href="data:image/x-icon;base64,AA" />
+        <Meta />
+        <Links />
+      </head>
+      <body>
+        <Outlet />
 
-    <Scripts />
-    </body>
+        <Scripts />
+      </body>
     </html>
   )
 }
